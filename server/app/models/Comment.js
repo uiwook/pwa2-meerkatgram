866b57462f5c052f5638c45b8f341cfd50208767
uiwook <dmldnr1230@yu.ac.kr>
2025-12-03 16:16:10 +0900

    251203 postShow API 수정

--- server/app/models/Comment.js
+++ server/app/models/Comment.js
@@ -95,6 +95,7 @@ const Comment = {
   associate: (db) => {
     db.Comment.belongsTo(db.User, { targetKey: 'id', foreignKey: 'userId', as: 'author'});
     db.Comment.belongsTo(db.Post, { targetKey: 'id', foreignKey: 'postId', as: 'posts'});
+    db.Comment.hasMany(db.Comment, { sourceKey: 'id', foreignKey: 'replyId', as: 'replies'});
   },
 }
 
