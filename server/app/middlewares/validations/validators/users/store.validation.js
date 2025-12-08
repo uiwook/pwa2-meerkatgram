8094dd1011669bd4ba084d6b409f16d6d1c0260d
uiwook <dmldnr1230@yu.ac.kr>
2025-12-08 13:21:27 +0900

    251208 users 주석 잊은거 기입

--- server/app/middlewares/validations/validators/users/store.validation.js
+++ server/app/middlewares/validations/validators/users/store.validation.js
@@ -1,3 +1,9 @@
+/**
+ * @file app/middlewares/validations/validators/store.validation.js
+ * @description 유저 회원가입 정보 검사 핸들러
+ * 251205 v1.0.0 wook init
+ */
+
 import users from '../../fields/user.field.js';
 
 export default [ users.email, users.password, users.profile, users.nick, users.passwordChk ];
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,3 +1,9 @@
+/**
+ * @file routes/users.router.js
+ * @description users 관련 라우터
+ * 251205 v1.0.0 wook init
+ */
+
 import express from 'express';
 import storeValidation from '../app/middlewares/validations/validators/users/store.validation.js';
 import validationHandler from '../app/middlewares/validations/validationHandler.js';
